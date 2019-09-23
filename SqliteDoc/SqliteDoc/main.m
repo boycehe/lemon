82abd04033c695adf29eda73f37c58aa728601f5
peilin.hpl <peilin.hpl@alibaba-inc.com>
2019-09-23 19:44:21 +0800

    init

---
+++ SqliteDoc/SqliteDoc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SqliteDoc
+//
+//  Created by heboyce on 2019/7/23.
+//  Copyright © 2019 heboyce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
